8a503e3d3da8b438e52c53c8514aa0e8554333e9
Vikash Kumar02 <vikash.kumar2@factset.com>
2019-04-11 01:53:44 +0530

    creating maven project for consistent hashing

---
+++ src/main/java/org/kid/system_design/App.java
@@ -0,0 +1,13 @@
+package org.kid.system_design;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
